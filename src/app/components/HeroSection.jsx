64192be0a71495a03af9d6927534580b766b9e5b
Zhaoyan Cong <congzhaoyan1999@gmail.com>
2024-02-13 22:27:15 +1100

    modified download CV button

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,7 +1,8 @@
-"use client"
+"use client";
 import React from 'react';
 import Image from 'next/image';
 import { TypeAnimation } from 'react-type-animation';
+import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/solid";
 
 const HeroSection = () => {
   return (
@@ -37,7 +38,10 @@ const HeroSection = () => {
                         Hire Me
                     </button>
                     <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
-                        <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Download CV</span>
+                        <span className="flex bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                            Download CV
+                            <ArrowTopRightOnSquareIcon className="rounded-full ml-1 w-5 h-5"/>
+                        </span>
                     </button>
                 </div>
             </div>
