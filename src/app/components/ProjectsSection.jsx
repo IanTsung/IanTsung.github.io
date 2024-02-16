661c7efc18c9489e610892fcb49ddbfa86de8856
Zhaoyan Cong <congzhaoyan1999@gmail.com>
2024-02-17 08:28:16 +1100

    added animation effect

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -1,5 +1,6 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+import { motion, useInView } from 'framer-motion';
 import ProjectCard from './ProjectCard';
 import ProjectTag from './ProjectTag';
 
@@ -17,6 +18,8 @@ const projectsData = [
 const ProjectsSection = () => {
 
 	const [tag, setTag] = useState("All");
+	const ref = useRef(null);
+	const isInView = useInView(ref, { once: true });
 
 	const handleTagChange = (newTag) => {
 		setTag(newTag);
@@ -26,8 +29,13 @@ const ProjectsSection = () => {
 		project.tag.includes(tag)
 	);
 
+	const cardVariants = {
+		initial: { y: 50, opacity: 0 },
+		animate: { y: 0, opacity: 1 }
+	}
+
 	return (
-		<>
+		<section>
 			<h2 className="text-center text-4xl font-bold text-white mt-4 mb-12">
 				Projects
 			</h2>
@@ -48,20 +56,31 @@ const ProjectsSection = () => {
 					isSelected={tag === "Mobile"}
 				/>
 			</div>
-			<div className="grid md:grid-cols-2 xl:grid-cols-3 gap-8 lg:gap-12">
+			<ul
+				ref={ref}
+				className="grid md:grid-cols-2 xl:grid-cols-3 gap-8 lg:gap-12"
+			>
 				{
-					filteredProjects.map((project) =>
-						<ProjectCard
-							key={project.id}
-							gitUrl={project.gitUrl}
-							imgUrl={project.image}
-							title={project.title}
-							description={project.description}
-						/>
+					filteredProjects.map((project, index) =>
+						<motion.li
+							key={index}
+							variants={cardVariants}
+							initial="initial"
+							animate={isInView ? "animate" : "initial"}
+							transition={{ duration: 0.3, delay: index * 0.4 }}
+						>
+							<ProjectCard
+								key={project.id}
+								gitUrl={project.gitUrl}
+								imgUrl={project.image}
+								title={project.title}
+								description={project.description}
+							/>
+						</motion.li>
 					)
 				}
-			</div>
-		</>
+			</ul>
+		</section>
 	)
 }
 
--- src/app/components/TabButton.jsx
+++ src/app/components/TabButton.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion';
+
+const variants = {
+	default: { width: 0 },
+	active: { width: "calc(100% - 0.75rem)" }
+}
 
 const TabButton = ({ active, selectTab, children }) => {
 
 	const buttonClasses = active
-		? "text-white border-b border-primary-500"
+		? "text-white"
 		: "text-[#ADB7BE]";
 
 	return (
@@ -11,6 +17,12 @@ const TabButton = ({ active, selectTab, children }) => {
 			<p className={`mr-3 font-semibold ${buttonClasses}`}>
 				{children}
 			</p>
+			<motion.div
+				animate={active ? "active" : "default"}
+				variants={variants}
+				className="h-1 bg-primary-500 mt-1 mr-3"
+			>
+			</motion.div>
 		</button>
 	)
 }
