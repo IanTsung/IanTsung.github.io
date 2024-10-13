6e7138f564a375c3f79a9e9f4f150a4e31b84bcf
Zhaoyan Cong <congzhaoyan1999@gmail.com>
2024-10-14 10:55:17 +1100

    restore metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 "use client";
+import { useEffect } from 'react';
 import { Montserrat } from "next/font/google";
 import { Provider } from 'react-redux';
 import { store } from './store';
@@ -7,6 +8,21 @@ import "./globals.css";
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
+
+  useEffect(() => {
+    document.title = 'Home | Ian Cong';
+
+    const metaDescription = document.querySelector('meta[name="description"]');
+    if (metaDescription) {
+      metaDescription.setAttribute('content', 'Ian\'s Personal Home Page');
+    } else {
+      const meta = document.createElement('meta');
+      meta.name = 'description';
+      meta.content = 'Ian\'s Personal Home Page';
+      document.head.appendChild(meta);
+    }
+  }, []);
+
   return (
     <Provider store={store}>
       <html lang="en">
