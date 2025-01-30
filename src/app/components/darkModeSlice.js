0f98f02af7a27838f1d58d7a2d9d3ed70ed1ebd4
Ian Cong <congzhaoyan1999@gmail.com>
2025-01-30 18:43:00 +1100

    update dark mode settings

--- src/app/components/darkModeSlice.js
+++ src/app/components/darkModeSlice.js
@@ -1,9 +1,16 @@
 "use client";
 import { createSlice } from '@reduxjs/toolkit';
 
+const getSystemColorScheme = () => {
+  if (typeof window !== 'undefined') {
+    return window.matchMedia('(prefers-color-scheme: dark)').matches;
+  }
+  return false;
+};
+
 const darkModeSlice = createSlice({
   name: 'darkMode',
-  initialState: false,
+  initialState: getSystemColorScheme(),
   reducers: {
     toggleDarkMode: (state) => !state,
   },
