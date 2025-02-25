4928bed25773a04aff8df04057c4f4802e0d38c3
Ian Cong <congzhaoyan1999@gmail.com>
2025-02-25 17:14:43 +1100

    remove reading dark mode from system

--- src/app/components/darkModeSlice.js
+++ src/app/components/darkModeSlice.js
@@ -1,16 +1,9 @@
 "use client";
 import { createSlice } from "@reduxjs/toolkit";
 
-const getSystemColorScheme = () => {
-  if (typeof window !== "undefined") {
-    return window.matchMedia("(prefers-color-scheme: dark)").matches;
-  }
-  return false;
-};
-
 const darkModeSlice = createSlice({
   name: "darkMode",
-  initialState: getSystemColorScheme(),
+  initialState: true,
   reducers: {
     toggleDarkMode: (state) => !state,
   },
